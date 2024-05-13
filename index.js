d06cd1be77729015dd204d9d0f7642516f2db10f
Farooque Ahmed <165314468+FarooqueAhmed1@users.noreply.github.com>
2024-05-13 16:25:52 +0500

    Sajjo

---
+++ index.js
@@ -0,0 +1,49 @@
+"use strict";
+// forEach
+// const names = [`Anas`, `Sajid`,`Nasoo`,`Farooque`]
+// const processedName =names.forEach((name:string)=> name+"processed")
+// console.log(processedName);
+Object.defineProperty(exports, "__esModule", { value: true });
+// map
+// const names = [`Anas`, `Sajid`,`Nasoo`,`Farooque`]
+// const processedName =names.map((name:string)=> name+"processed")
+// console.log(processedName);
+// join
+// const names = [`Anas`, `Sajid`,`Nasoo`,`Farooque`]
+// const processedName =names.join('a')
+// console.log(processedName)
+// Split
+// const names = "Anas-Sajid-Nasoo-Farooque"
+// const processedName =names.split('-')
+// console.log(processedName)
+// Count Sentence words
+// const para = "My name is Sajid Rajper"
+// console.log(para.split(" ").length)
+// // Convert sentence into words
+// const para = "My name is Sajid Rajper"
+// console.log(para.split(" "))
+// Concat
+// let students = ['Fahad','Sajid']
+// let newStudents = ['Hamza','Farooque']
+// let updatedStudents = students.concat(newStudents)
+// console.log(updatedStudents)
+// Flat
+// let students = ['Ali','Bilal',['Shuja','Qurban']]
+// console.log(students.flat())
+// let students = ['Ali','Bilal',['Shuja','Qurban',['Sajid','Nasir']]]
+// console.log(students.flat())
+// Object => means print all thing in one command like name,age ETC
+// const student = {
+//    name:"Abdullah",
+//    stdId: "83987",
+//    fatherName: "Gaffar",
+//    age: 21,
+// }
+// // console.log(students)
+// console.log(student.stdId)
+// console.log(student["name"])
+// console.log(student["father.Name"])    //<= for special character we use square brackets just like for "Dot", "Space" ETC
+// console.log(student.qualification.grade)
+// console.log(student['qualification']['grade'])
+// console.log(student["qualification"].grade)
+// console.log(student.qualification["result"])
