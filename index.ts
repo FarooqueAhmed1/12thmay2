5c381514c9e85126c799b9eb1a8ea98c3ed2c21e
Farooque Ahmed <165314468+FarooqueAhmed1@users.noreply.github.com>
2024-05-23 22:12:01 +0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -9,8 +9,8 @@
 // console.log(processedName);
 
 // join
-// const names = [`Anas`, `Sajid`,`Nasoo`,`Farooque`]
-// const processedName =names.join('a')
+// const names = [`Anas`, `Sajid`,`Nasoo`,`Farooque`,`danish`]
+// const processedName =names.join('SHEIKH')
 // console.log(processedName)
 
 
@@ -66,4 +66,71 @@
 // console.log(student["qualification"].grade)
 // console.log(student.qualification["result"])
 
+// Sajid Practice
+// let a=[`sajid`,`farooque`]
+// console.log(a.join(`Rajper`))
 
+
+
+
+
+
+
+
+
+
+
+
+// 19th May Practice
+
+// Arrow Function
+// function checkSalary(salary) {
+//     return.salary > 50000
+// }
+// (salary) => salary > 50000;
+
+// let a = {
+//     name: '',
+//     fatherName: '',
+//     studentId: ''
+// }
+// let b = {
+//     name: '',
+//     fatherName: '',
+//     age: ''
+// }
+
+interface Employee {
+    employeeId: number;
+    employeeName: string;
+    salary: number;
+}
+let collectionofEmployee: Employee[] = [
+    {
+        employeeId: 83943,
+        employeeName: "Ali Sheikh",
+        salary: 105000
+    },
+    {
+        employeeId: 174005,
+        employeeName: "Sajid Rajper",
+        salary: 95000
+    },{
+        employeeId: 117923,
+        employeeName: "Salam Sheikh",
+        salary: 75000
+    },
+];
+// let filteredDatabySalaryRange = collectionofEmployee.filter();
+
+// Arrow Function
+// let filterBySalary = collectionofEmployee.filter((employee) => employee.salary > 50000);
+// let filterBySalaryId = collectionofEmployee.filter((employee: Employee)=> employee.employeeId == 83943)
+// // let filterBySalaryName = collectionofEmployee.filter((employee: Employee)=> employee.employeeName == "Sajid")
+// collectionofEmployee[0].employeeName.includes
+let filterBySalaryName = collectionofEmployee.filter((employee: Employee)=> employee.employeeName.includes("Sajid"))
+// Basic Arrow funtion code
+// function filterBySalary(employee: Employee){
+//     return employee.salary > 50000;
+// }
+// (employee) => employee.salary > 50000;
